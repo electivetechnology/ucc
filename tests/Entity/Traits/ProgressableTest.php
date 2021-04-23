2144aa1d6e31a0d0b5038969ce8e205e88998de7
Chris Dixon <chris.js.dixon@gmail.com>
2021-04-23 11:16:42 +0100

    test

---
+++ tests/Entity/Traits/ProgressableTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ElectiveGroup\Ucc\Tests\Entity\Traits;
+
+use ElectiveGroup\Ucc\Entity\Traits\Progressable;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * ElectiveGroup\Ucc\Tests\Entity\Traits\ProgressableTest
+ *
+ * @author Chris Dixon <chris@electivegroup.com>
+ */
+class ProgressableTest extends TestCase
+{
+    public function testIsCreatable()
+    {
+        $progress = 'completed';
+        $mock   = $this->getMockForTrait(Progressable::class);
+
+        $this->assertInstanceOf(get_class($mock), $progress);
+        $this->assertSame($progress, $mock->getCreatedAt());
+        $this->assertTrue(property_exists(Progressable::class, "progress"));
+    }
+}
